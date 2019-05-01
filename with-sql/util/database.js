60ad39ceaf5e65856a49e2622d8a126f93bfa1c3
Pritam Kumar <pk.rajece2010@gmail.com>
2019-05-01 18:25:59 +0530

    Final SQL database integration

---
+++ with-sql/util/database.js
@@ -0,0 +1,7 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "12345", {
+  dialect: "mysql",
+  host: "localhost"
+});
+module.exports = sequelize;
